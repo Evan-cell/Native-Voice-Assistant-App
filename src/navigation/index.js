79804053f043ab8e4a409a39c847302137511dc0
evan <malcomchege0582@gmail.com>
2023-10-28 00:08:55 +0300

    chart app with chat gpt and dall-e

---
+++ src/navigation/index.js
@@ -0,0 +1,22 @@
+import react from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation(){
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown:false}}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Welcome" component={WelcomeScreen} />
+                
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default AppNavigation;
